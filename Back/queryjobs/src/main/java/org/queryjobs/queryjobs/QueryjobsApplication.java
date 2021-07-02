faad922c2dda271aa5884581d994a9e09eac9009
Jao <joaovictorjgar@gmail.com>
2021-07-02 10:05:40 -0300

    restaurando tudo
    add todo o cod de novo por conta do erro da src

---
+++ Back/queryjobs/src/main/java/org/queryjobs/queryjobs/QueryjobsApplication.java
@@ -0,0 +1,13 @@
+package org.queryjobs.queryjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryjobsApplication.class, args);
+	}
+
+}
